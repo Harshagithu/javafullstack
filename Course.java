92e366529700191ad83e3c6770d682afc4397caa
Harshagithu <gottipatiharsha21@gmail.com>
2026-03-16 10:21:33 +0530

    New Commit;

--- Course.java
+++ Course.java
@@ -8,13 +8,22 @@ class Course
 
     public static void main(String args[])
     {
-       //Course object
-       Course courseRef=new Course();
-       courseRef.courseId=1234;
-       courseRef.courseName="Java Full Stack";
-       courseRef.courseDuration=100;
-       System.out.println("Course Id: "+courseRef.courseId);
-       System.out.println("Course Name: "+courseRef.courseName);
-       System.out.println("course Duration: "+courseRef.courseDuration);
+       //Course object 1
+       Course courseRef1=new Course();
+       courseRef1.courseId=1234;
+       courseRef1.courseName="Java Full Stack";
+       courseRef1.courseDuration=100;
+       System.out.println("Course1 Id: "+courseRef1.courseId);
+       System.out.println("Course1 Name: "+courseRef1.courseName);
+       System.out.println("course1 Duration: "+courseRef1.courseDuration);
+
+       //Course object 2
+       Course courseRef2=new Course();
+       courseRef2.courseId=4321;
+       courseRef2.courseName="Python Full Stack";
+       courseRef2.courseDuration=100;
+       System.out.println("\nCourse2 Id: "+courseRef2.courseId);
+       System.out.println("Course2 Name: "+courseRef2.courseName);
+       System.out.println("course2 Duration: "+courseRef2.courseDuration);
     }
 }
